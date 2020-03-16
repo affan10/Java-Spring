49c086b187d30ccfdb12d72dd97ce68dea76d4e5
Affan Muddassir <affan.muddassir10@gmail.com>
2020-03-16 21:04:29 +0500

    Add files via upload

---
+++ Inner Beans, Aliases and idref/SpringPractice.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package springpractice;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author affan
+ */
+public class SpringPractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("project.xml");
+        Triangle triangleObj = (Triangle) context.getBean("Triangle");
+        triangleObj.draw();
+    }
+    
+}
---
+++ Inner Beans, Aliases and idref/Triangle.java
@@ -0,0 +1,47 @@
+package springpractice;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author affan
+ */
+public class Triangle {
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
+
+    public Point getPointA() {
+        return pointA;
+    }
+
+    public Point getPointB() {
+        return pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
+    }
+
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
+    }
+    
+    public void draw() {
+        System.out.println("Point A = (" + getPointA().getX() + ", " + getPointA().getY() + ")");
+        System.out.println("Point B = (" + getPointB().getX() + ", " + getPointB().getY() + ")");
+        System.out.println("Point C = (" + getPointC().getX() + ", " + getPointC().getY() + ")");
+    }
+}
