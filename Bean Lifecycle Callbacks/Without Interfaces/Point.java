e13f0b1ed80d92a4cdd8c12c80cd66016e3f06bd
Affan Muddassir <affan.muddassir10@gmail.com>
2020-03-23 13:45:48 +0500

    Add files via upload

---
+++ Bean Lifecycle Callbacks/Without Interfaces/Point.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package springpractice;
+
+/**
+ *
+ * @author affan
+ */
+public class Point {
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+}
---
+++ Bean Lifecycle Callbacks/Without Interfaces/SpringPractice.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package springpractice;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author affan
+ */
+public class SpringPractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("project.xml");
+        context.registerShutdownHook();
+        Triangle triangleObj = (Triangle) context.getBean("Triangle2");
+        triangleObj.draw();
+    }
+    
+}
