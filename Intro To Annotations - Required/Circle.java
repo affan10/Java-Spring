7c8d95016d3922148cdbbf026de73fb1f2e3d038
Affan Muddassir <affan.muddassir10@gmail.com>
2020-03-24 16:38:59 +0500

    Add files via upload

---
+++ Intro To Annotations - Required/Circle.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package springpractice;
+
+import org.springframework.beans.factory.annotation.Required;
+
+/**
+ *
+ * @author affan
+ */
+public class Circle implements Shape {
+    private Point center;
+
+    public Point getCenter() {
+        return center;
+    }
+
+    @Required
+    public void setCenter(Point center) {
+        this.center = center;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing Triangle...\nCenter Point = (" + getCenter().getX() + ", " + getCenter().getY() + ")");
+    }
+    
+}
