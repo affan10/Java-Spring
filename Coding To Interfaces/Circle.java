c8ad7867af2be156c6021e2a12cfaee47ae986f7
Affan Muddassir <affan.muddassir10@gmail.com>
2020-03-24 16:19:54 +0500

    Add files via upload

---
+++ Coding To Interfaces/Circle.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package springpractice;
+
+/**
+ *
+ * @author affan
+ */
+public class Circle implements Shape {
+    private Point center;
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public void setCenter(Point center) {
+        this.center = center;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing Triangle...\nCenter Point = (" + getCenter().getX() + ", " + getCenter().getY() + ")");
+    }
+    
+}
